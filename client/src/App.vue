428d5d5b1b1225de39214b123580670831936ec2
heather bickish <hbickish@gmail.com>
2024-12-18 10:03:32 -0700

    got star map working

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,6 @@
 <script setup>
 import Navbar from './components/Navbar.vue';
 import { AppState } from './AppState.js';
-
 </script>
 
 <template>
@@ -11,20 +10,10 @@ import { AppState } from './AppState.js';
   <main>
     <router-view />
   </main>
-  <footer class="bg-dark text-light">
-    Made with 💖 by CodeWorks
+  <footer>
   </footer>
 </template>
 
 <style lang="scss">
 @import '@/assets/scss/main.scss';
-:root {
-  --main-height: calc(100vh - 32px - 64px);
-}
-
-footer {
-  display: grid;
-  place-content: center;
-  height: 32px;
-}
 </style>
