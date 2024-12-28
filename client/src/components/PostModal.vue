1fd313be50934be1e5ee7409317ef0bb79c30ddd
Pai <>
2024-12-28 08:40:08 -0700

    Modified the size of the modal

--- client/src/components/PostModal.vue
+++ client/src/components/PostModal.vue
@@ -1,51 +1,64 @@
 <script setup>
-import { postsService } from '@/services/PostsService';
-import Pop from '@/utils/Pop';
-import { Modal } from 'bootstrap';
-import { ref } from 'vue';
-import { useRoute } from 'vue-router';
+import { postsService } from "@/services/PostsService";
+import { logger } from "@/utils/Logger";
+import Pop from "@/utils/Pop";
+import { Modal } from "bootstrap";
+import { ref } from "vue";
+import { useRoute } from "vue-router";
 
-const route = useRoute()
+const route = useRoute();
 
 const editablePostData = ref({
-    body: '',
-    imgUrl: '',
-    groupId: route.params.groupId
-
-})
+  body: "",
+  imgUrl: "",
+//   image: null,
+  groupId: route.params.groupId,
+});
 
 async function createPost() {
-    try {
-        await postsService.createPost(editablePostData.value)
-        editablePostData.value.body = ''
-        editablePostData.value.imgUrl = ''
-        Modal.getInstance('#postModal').hide()
-    }
-    catch (error) {
-        Pop.error(error);
-    }
-
+  try {
+    await postsService.createPost(editablePostData.value);
+    editablePostData.value.body = "";
+    // editablePostData.value.image = null;
+    editablePostData.value.imgUrl = "",
+    Modal.getInstance("#postModal").hide();
+  } catch (error) {
+    Pop.error(error);
+  }
 }
-
+// function uploadFile(event){
+//     console.log(event.target.files["0"]["name"])
+//     editablePostData.value.imgUrl = event.target.files["0"]["name"]
+// }
 </script>
 
 <template>
-    <form @submit.prevent="createPost()">
-        <div class="form-floating mb-3 textarea">
-            <textarea v-model="editablePostData.body" maxlength="1000" rows="10" class="form-control" id="body"
-                name="body" placeholder="Write something" required></textarea>
+  <form @submit.prevent="createPost()">
+    <div class="mb-3">
+      <textarea v-model="editablePostData.body" maxlength="1000" class="form-control p-0" name="body" placeholder="Create a post..." rows="10" required></textarea>
+    </div>
+    <!-- <div class="form-floating mb-3">
+        <div>
+            <img src="https://static.xx.fbcdn.net/rsrc.php/v4/yQ/r/74AG-EvEtBm.png" alt="">
         </div>
-        <div class="form-floating mb-3">
-            <input v-model="editablePostData.imgUrl" type="url" id="imgUrl" name="img-url" placeholder="Url">
-        </div>
-        <div class="text-end">
-            <button type="submit">Submit</button>
-        </div>
-    </form>
+        <div>
+            <input @change="uploadFile($event)" type="file" name="file">
+        </div> -->
+      <input
+        v-model="editablePostData.imgUrl"
+        type="url"
+        id="imgUrl"
+        name="img-url"
+        placeholder="Url"
+      />
+    <div class="text-end">
+      <button type="submit">Submit</button>
+    </div>
+  </form>
 </template>
 
 <style scoped lang="scss">
 textarea {
-    resize: none;
+  resize: none;
 }
 </style>
