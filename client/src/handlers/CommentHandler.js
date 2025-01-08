aed0c623268210c9d97930f300fdf10d6eed4dcd
Pai <>
2025-01-08 16:45:58 -0700

    Socket part 2

--- client/src/handlers/CommentHandler.js
+++ client/src/handlers/CommentHandler.js
@@ -1,9 +1,15 @@
+import { AppState } from "@/AppState.js";
+import { Comment } from "@/models/Comment.js";
 import { SocketHandler } from "@/utils/SocketHandler.js";
 
 class CommentHandler extends SocketHandler{
   constructor(){
     super()
-    this.on('')
+    this.on('CREATED_COMMENT', this.onCreatedComment)
+  }
+  onCreatedComment(payload){
+    const comment = new Comment(payload);
+    AppState.comments.push(comment);
   }
 }
 export const commentHandler = new CommentHandler();
