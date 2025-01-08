dee1d106aedbf2aad9cca97a160d837424729a31
heather bickish <hbickish@gmail.com>
2025-01-08 14:51:07 -0700

    added group id to event schema

--- server/src/models/CreatorEvent.js
+++ server/src/models/CreatorEvent.js
@@ -13,20 +13,22 @@ const pointSchema = new Schema({
 });
 
 
-export const CreatorEventSchema = new Schema({
-  creatorId: { type: Schema.ObjectId, ref: 'Account', required: true },
-  title: { type: String, maxLength: 100, minLength: 3, required: true },
-  description: { type: String, maxLength: 2000, minLength: 15, required: true },
-  location: {
-    type: pointSchema,
-    required: true
+export const CreatorEventSchema = new Schema(
+  {
+    groupId: { type: Schema.ObjectId, ref: 'Group', required: true },
+    creatorId: { type: Schema.ObjectId, ref: 'Account', required: true },
+    title: { type: String, maxLength: 100, minLength: 3, required: true },
+    description: { type: String, maxLength: 2000, minLength: 15, required: true },
+    location: {
+      type: pointSchema,
+      required: true
+    },
   },
-
-},
   {
     timestamps: true,
     toJSON: { virtuals: true }
-  })
+  }
+)
 
 CreatorEventSchema.virtual('creator', {
   localField: 'creatorId',
